4f824c35be05c204f47100fa29eaba16f460a855
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2022-11-25 11:45:35 +0300

    Initial implementation for aperture_filter

---
+++ example/aperture_filter.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <boost/program_options.hpp>
+
+#include <xtensor/xarray.hpp>
+#include <xtensor/xbuilder.hpp>
+#include <xtensor/xcsv.hpp>
+#include <xtensor/xmanipulation.hpp>
+
+#include <aperture_filter.h>
+
+template<class AF>
+void dump_aperture_filter(const std::string& filename, const AF& af, std::size_t size) {
+	xt::xarray<float> grid = xt::linspace(static_cast<float>(0), static_cast<float>(5), size);
+
+	std::ofstream stm(filename);
+	xt::dump_csv(stm, xt::transpose(xt::vstack(xt::xtuple(grid, af(grid)))));
+}
+
+
+int main(int argc, char** argv) {
+	namespace po = boost::program_options;
+
+	po::options_description opts;
+	po::positional_options_description pos_opts;
+	po::variables_map va;
+
+	opts.add_options()
+		("size", po::value<std::size_t>()->default_value(1024), "Output grid size");
+
+	try {
+		auto parsed = po::command_line_parser(argc, argv).options(opts).positional(pos_opts).run();
+		po::store(std::move(parsed), va);
+		po::notify(va);
+
+		dump_aperture_filter("circular_aperture.csv", weif::circular_aperture<float>{}, va["size"].as<std::size_t>());
+		dump_aperture_filter("annular_aperture.csv", weif::annular_aperture<float>{0.25}, va["size"].as<std::size_t>());
+
+	} catch (const po::error& e) {
+		std::cerr << e.what() << std::endl;
+		std::cerr << opts << std::endl;
+
+		return 1;
+	}
+
+	return 0;
+}
---
+++ include/aperture_filter.h
@@ -0,0 +1,93 @@
+#ifndef _WEIF_APERTURE_FILTER_H
+#define _WEIF_APERTURE_FILTER_H
+
+#include <cmath>
+#include <type_traits>
+
+#include <boost/math/tools/precision.hpp>
+
+#include <xtensor/xmath.hpp>
+#include <xtensor/xvectorize.hpp>
+
+
+namespace weif {
+
+namespace detail {
+
+template<class T>
+inline T airy_tmp(const T x) noexcept {
+	using namespace std;
+
+	if (abs(x) >= 3.7 * boost::math::tools::forth_root_epsilon<T>()) {
+		return cyl_bessel_j(1, x) / x * static_cast<T>(2);
+	} else {
+        	// |x| < (eps*192)^(1/4)
+		return static_cast<T>(1) - x * x / 8;
+	}
+}
+
+} // detail
+
+
+template<class T>
+struct circular_aperture {
+	using value_type = T;
+
+	value_type operator() (value_type u) const noexcept {
+		return std::pow(detail::airy_tmp(xt::numeric_constants<value_type>::PI * u), 2);
+	}
+
+	template<class E>
+	auto operator() (const xt::xexpression<E>& e) const noexcept {
+		auto airy_vec = xt::vectorize(&detail::airy_tmp<value_type>);
+
+		return xt::square(airy_vec(xt::numeric_constants<value_type>::PI * e.derived_cast()));
+	}
+
+	template<class E1, class E2>
+	auto operator() (const xt::xexpression<E1>& e1, const xt::xexpression<E1>& e2) const noexcept {
+		return this->operator()(xt::sqrt(xt::square(e1.derived_cast()) + xt::expand_dims(xt::square(e2.derived_cast()),1)));
+	}
+};
+
+template<class T>
+class annular_aperture {
+public:
+	using value_type = T;
+
+private:
+	value_type obscuration_;
+
+public:
+	explicit annular_aperture(value_type obscuration) noexcept:
+		obscuration_{obscuration} {}
+
+	const auto& obscuration() const noexcept { return obscuration_; }
+
+	value_type operator() (value_type u) const noexcept {
+		const auto eps2 = std::pow(obscuration(), 2);
+		const auto norm = std::pow(static_cast<value_type>(1) - eps2, 2);
+		const auto piu = xt::numeric_constants<value_type>::PI * u;
+
+		return std::pow(detail::airy_tmp(piu) - eps2 * detail::airy_tmp(obscuration() * piu), 2) / norm;
+	}
+
+	template<class E>
+	auto operator() (const xt::xexpression<E>& e) const noexcept {
+		const auto eps2 = std::pow(obscuration(), 2);
+		const auto norm = std::pow(static_cast<value_type>(1) - eps2, 2);
+
+		auto airy_vec = xt::vectorize(&detail::airy_tmp<value_type>);
+
+		return xt::square(airy_vec(xt::numeric_constants<value_type>::PI * e.derived_cast()) - eps2 * airy_vec((xt::numeric_constants<value_type>::PI * obscuration()) * e.derived_cast())) / norm;
+	}
+
+	template<class E1, class E2>
+	auto operator() (const xt::xexpression<E1>& e1, const xt::xexpression<E1>& e2) const noexcept {
+		return this->operator()(xt::sqrt(xt::square(e1.derived_cast()) + xt::expand_dims(xt::square(e2.derived_cast()),1)));
+	}
+};
+
+} // weif
+
+#endif // _WEIF_APERTURE_FILTER_H
